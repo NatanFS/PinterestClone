7a2ac33170fc6b5caacf2442f9aac4ed416da0e7
Natan Felipe de Freitas Souza <natanlipf@gmail.com>
2024-05-21 07:18:44 -0300

    fix: cors errors, returning serialized user

--- src/pins/serializers.py
+++ src/pins/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Pin
+from pins.models import Pin
+from users.serializer import UserSerializer
 
 class PinSerializer(serializers.ModelSerializer):
 
@@ -7,6 +8,11 @@ class PinSerializer(serializers.ModelSerializer):
         model = Pin
         fields = '__all__'
         read_only_fields = ['user']
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['user'] = UserSerializer(instance.user).data
+        return response
 
 class PinLikeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
